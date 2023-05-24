12c99a432c78bd383febba0479187fae76e19caf
A Stone <astone@heckgrammar.co.uk>
2023-05-24 09:47:46 +0100

    Add project files.

---
+++ CountdownY9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountdownY9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r=new Random();
+            string e = "3*4/2";
+            string result = new DataTable().Compute(e,"").ToString();
+            Console.WriteLine(result);
+            int[] large = new int[] { 100, 75, 50, 25 };
+            int[] small = new int[20];
+            int n = 1;
+            for (int i = 0; i < small.Length; i+=2)
+            {
+                small[i] = n;
+                small[i+1] = n;
+                n++;
+            }
+            int largeCount=0;
+            int smallCount=0;
+            while(largeCount +  smallCount != 6)
+            {
+                Console.Write("How many large: ");
+                largeCount = Convert.ToInt32(Console.ReadLine());
+                Console.Write("How many small: ");
+                smallCount = Convert.ToInt32(Console.ReadLine());
+                if (largeCount > 4)
+                {
+                    Console.WriteLine("There are only 4 large numbers!");
+                }
+                else
+                {
+                    if (largeCount + smallCount != 6) { Console.WriteLine("You must have 6 numbers only"); }
+                }
+            }
+
+        }
+    }
+}
---
+++ CountdownY9/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CountdownY9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HGS-SCCM")]
+[assembly: AssemblyProduct("CountdownY9")]
+[assembly: AssemblyCopyright("Copyright © HGS-SCCM 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99395688-1055-41a0-a6ef-22cab9502001")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
