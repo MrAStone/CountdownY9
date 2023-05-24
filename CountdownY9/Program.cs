158aae45533024d5151c137b23a2aa2be178fd7f
A Stone <astone@heckgrammar.co.uk>
2023-05-24 09:58:58 +0100

    FullyWorking Ish

--- CountdownY9/Program.cs
+++ CountdownY9/Program.cs
@@ -12,9 +12,7 @@ namespace CountdownY9
         static void Main(string[] args)
         {
             Random r=new Random();
-            string e = "3*4/2";
-            string result = new DataTable().Compute(e,"").ToString();
-            Console.WriteLine(result);
+            
             int[] large = new int[] { 100, 75, 50, 25 };
             int[] small = new int[20];
             int n = 1;
@@ -40,8 +38,55 @@ namespace CountdownY9
                 {
                     if (largeCount + smallCount != 6) { Console.WriteLine("You must have 6 numbers only"); }
                 }
-            }
 
+            }
+            // shuffle large
+            for(int i = 0; i < 1000; i++)
+            {
+                int a = r.Next(0, 4);
+                int b = r.Next(0,4);
+                int temp = large[a];
+                large[a] = large[b];
+                large[b] = temp;
+            }
+            // shuffle small
+            for (int i = 0; i < 1000; i++)
+            {
+                int a = r.Next(0, 20);
+                int b = r.Next(0, 20);
+                int temp = small[a];
+                small[a] = small[b];
+                small[b] = temp;
+            }
+            int[] numbers = new int[6];
+            int index = 0;
+            for (int i = 0;i< largeCount;i++)
+            {
+                numbers[index++] = large[i];
+            }
+            for (int i = 0;i< smallCount;i++)
+            {
+                numbers[index++]= small[i];
+            }
+            foreach(int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            int numToGet = r.Next(100, 1000);
+            Console.WriteLine(numToGet);
+            Console.Write("Enter your answer: ");
+            string answer = Console.ReadLine();
+            string result = new DataTable().Compute(answer, "").ToString();
+            Console.WriteLine(result);
+            if(result == numToGet.ToString())
+            {
+                Console.WriteLine("WINNER");
+            }
+            else
+            {
+                Console.WriteLine("LOOSER!");
+            }
         }
     }
 }
